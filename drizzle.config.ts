1a88bc31ac4930c6df3e60c6e1530fe6123a22a1
Omji Kushwaha <34828782+omjikush09@users.noreply.github.com>
2025-05-22 17:37:32 +0530

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,6 +6,6 @@ export default defineConfig({
 	out: "./migrations",
 	dialect: "postgresql",
 	dbCredentials: {
-		url: "postgresql://bank_owner:npg_rxXe0pbCFZc2@ep-sparkling-mountain-a1a249wq-pooler.ap-southeast-1.aws.neon.tech/bank?sslmode=require",
+		url: process.env.DATABASE_URL,
 	},
 });
